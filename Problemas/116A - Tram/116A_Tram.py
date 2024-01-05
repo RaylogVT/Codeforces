772ac54db8cd3c726f4f725472bc6013cead534d
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-05 12:27:26 -0600

    Update 116A_Tram.py

--- Problemas/116A - Tram/116A_Tram.py
+++ Problemas/116A - Tram/116A_Tram.py
@@ -1,8 +1,8 @@
 # 116A - Tram
 # https://codeforces.com/problemset/problem/116/A
 
-# Inputs:
-# 1) Número de estaciones
+# Input:
+# 1) estaciones = Número de estaciones
 estaciones = int(input())
 
 # Número máximo de pasajeros que el tren ha tenido hasta el momento
@@ -14,9 +14,9 @@ pasajeros_actuales = 0
 # Recorre cada estación
 for estacion in range(0,estaciones):
 
-    # Inputs:
-    # 2) Número de pasajeros que salen del tren
-    # 3) Número de pasajeros que entran al tren
+    # Input:
+    # 2) pasajeros_salientes = Número de pasajeros que salen del tren
+    # 3) pasajeros_entrantes = Número de pasajeros que entran al tren
     pasajeros_salientes, pasajeros_entrantes = map(int, input().split())
 
     # Calcula cuántos pasajeros tiene el tren después de que salieran y entraran los correspondientes
@@ -27,4 +27,5 @@ for estacion in range(0,estaciones):
     if pasajeros_actuales > maximo_pasajeros:
         maximo_pasajeros = pasajeros_actuales
 
+# Imprime el número máximo de pasajeros que hubo en el tren
 print(maximo_pasajeros)
