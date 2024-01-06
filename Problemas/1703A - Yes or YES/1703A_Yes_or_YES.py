005071cd8a37f6771c3237340aec2d67c001eff8
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-05 19:18:05 -0600

    Update 1703A_Yes_or_YES.py

--- Problemas/1703A - Yes or YES/1703A_Yes_or_YES.py
+++ Problemas/1703A - Yes or YES/1703A_Yes_or_YES.py
@@ -2,14 +2,14 @@
 # https://codeforces.com/problemset/problem/1703/A
 
 # Input:
-# 1) Número de pruebas
+# 1) pruebas = Número de pruebas
 pruebas = int(input())
 
 # Realiza cada caso de prueba
 for prueba in range(0, pruebas):
 
     # Input:
-    # 2) String de 3 caracteres
+    # 2) yes = String de 3 caracteres
     yes = input()
 
     # Si el string en minúsculas es igual a "yes", entonces imprime "YES", de lo contrario, imprime "NO"
