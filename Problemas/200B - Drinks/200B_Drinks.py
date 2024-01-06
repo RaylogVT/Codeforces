b18fba81ab88fc497b6bcdef203e73f25c0ed2b5
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-05 19:30:25 -0600

    Update 200B_Drinks.py

--- Problemas/200B - Drinks/200B_Drinks.py
+++ Problemas/200B - Drinks/200B_Drinks.py
@@ -2,11 +2,13 @@
 # https://codeforces.com/problemset/problem/200/B
 
 # Inputs:
-# 1) Número de bebidas
-# 2) % de jugo de naranja en cada bebida
+# 1) bebidas = Número de bebidas
+# 2) porcentaje_jugo_bebidas = % de jugo de naranja en cada bebida
 bebidas = int(input())
 porcentaje_jugo_bebidas = list(map(int, input().split()))
 
 # Saca el promedio de los porcentajes de jugo de naranja de las bebidas
 porcentaje_jugo_mezcla = sum(porcentaje_jugo_bebidas)/len(porcentaje_jugo_bebidas)
+
+# Imprime el promedio de porcentaje de jugo
 print(porcentaje_jugo_mezcla)
