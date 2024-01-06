b0d8462ded585f70df9a97bd42aec94d376a86f1
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-05 18:50:30 -0600

    Update 151A_Soft_Drinking.cpp

--- Problemas/151A - Soft Drinking/151A_Soft_Drinking.cpp
+++ Problemas/151A - Soft Drinking/151A_Soft_Drinking.cpp
@@ -10,25 +10,25 @@ using namespace std;
 int main(){
 
     // Input:
-    // 1) Número de amigos
-    // 2) Número de botellas
-    // 3) Mililitros por botella
-    // 4) Número de limones
-    // 5) Número de trozos de limones
-    // 6) Gramos de sal
-    // 7) Mínimo de mililitros para brindas
-    // 8) Mínimo de sal para brindas
-    int amigos, botellas, mililitros, limones, trozos, sal, minimo_mililitros, minimo_sal, bebidas_totales;
-    cin >> amigos >> botellas >> mililitros >> limones >> trozos >> sal >> minimo_mililitros >> minimo_sal;
+    // 1) amigos = Número de amigos
+    // 2) botellas = Número de botellas
+    // 3) mililitros = Mililitros por botella
+    // 4) limones = Número de limones
+    // 5) trozos = Número de trozos de limones
+    // 6) gramos_sal = Gramos de sal
+    // 7) minimo_mililitros = Mínimo de mililitros para brindas
+    // 8) minimo_sal ) Mínimo de sal para brindas
+    int amigos, botellas, mililitros, limones, trozos, gramos_sal, minimo_mililitros, minimo_sal, bebidas_totales;
+    cin >> amigos >> botellas >> mililitros >> limones >> trozos >> gramos_sal >> minimo_mililitros >> minimo_sal;
 
-    // Calcula el número de brindis que se pueden realizar con los mililitros de bebida disponibles
+    // Calcula el número de brindis que se pueden realizar con los mililitros de alcohol disponibles
     int brindis_alcohol = (botellas * mililitros) / minimo_mililitros;
 
     // Calcula el número de brindis que se pueden realizar con los trozos de limones disponibles
     int brindis_limones = limones * trozos;
 
     // Calcula el número de brindis que se pueden realizar con los gramos de sal disponibles
-    int brindis_sal = sal / minimo_sal;
+    int brindis_sal = gramos_sal / minimo_sal;
 
     // Calcula el número de bebidas que se pueden hacer
     // El número de bebidas está determinado por el valor mínimo de bebidas, limones y sal disponible
