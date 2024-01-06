58864cfdfb70521017a2f89de30edede4c8deb01
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-05 19:21:54 -0600

    Update 1742A_Sum.cpp

--- Problemas/1742A - Sum/1742A_Sum.cpp
+++ Problemas/1742A - Sum/1742A_Sum.cpp
@@ -24,6 +24,7 @@ int main(){
 
         // Checa si al sumar dos pares de números dan como resultado el tercero
         // Prueba todas las combinaciones de pares de números
+        // Imprime "YES" si dos números suman el tercero, de lo contrario, imprime "NO"
         if(a == b + c || b == a + c || c == a + b) {
             cout << "YES" << endl;
         } else {
