74232e13cc89eec6b8ddbee2512bfc573c55b4f4
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 20:27:54 -0600

    Update 41A_Translation.py

--- Problemas/41A - Translation/41A_Translation.py
+++ Problemas/41A - Translation/41A_Translation.py
@@ -2,23 +2,23 @@
 # https://codeforces.com/problemset/problem/41/A
 
 # Inputs
-# 1) Palabra en Berland
-# 2) Palabra en Birland
-berland_s = input()
-birland_t = input()
+# 1) berland = Palabra en Berland
+# 2) birland = Palabra en Birland
+berland = input()
+birland = input()
 
 # Palabra en Berland traducida a Birland (invertida)
 traduccion_berland_birland = ""
 
 # Tamaño de la palabra en Berland
-tamano = len(berland_s)
+tamano = len(berland)
 
 # Lee la palabra en Berland de derecha a izquierda, añade letra por letra a la traducción 
 for letra in range(tamano-1, -1, -1):
-    traduccion_berland_birland = traduccion_berland_birland + berland_s[letra]
+    traduccion_berland_birland = traduccion_berland_birland + berland[letra]
 
 # Si la traducción es igual a la palabra en Birland, entonces imprime YES, de lo contrario, imprime NO
-if traduccion_berland_birland == birland_t:
+if traduccion_berland_birland == birland:
     print('YES')
 else:
     print('NO')
