9dcfaa99c7202f38beccdcee70463fe76d777612
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 20:48:39 -0600

    Update 263A_Beautiful_Matrix.py

--- Problemas/263A - Beautiful Matrix/263A_Beautiful_Matrix.py
+++ Problemas/263A - Beautiful Matrix/263A_Beautiful_Matrix.py
@@ -1,19 +1,19 @@
 # 263A - Beautiful Matrix
 # https://codeforces.com/problemset/problem/263/A
 
-for i in range(0,5): 
+# Itera sobre las 5 filas de la matriz
+for fila in range(0,5): 
 
     # Input:
-    # 1) Matriz de 0s y 1
-    # Rellena cada fila
-    fila = input().split() 
+    # 1) columna = Columna de matriz
+    columna = input().split() 
 
     # Si la fila tiene el 1, extrae su índice
-    if '1' in fila:
-        x = i
+    if '1' in columna:
+        x = fila
 
         # Extrae el índice de la columna donde está el 1
-        y = fila.index('1')
+        y = columna.index('1')
 
 # Calcula el número de movimientos
 movimientos = abs(x-2) + abs(y-2)
