0993f22ad94ae7f1212e254619829ed1a1d4a31a
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 18:59:24 -0600

    Update 723A_New_Year_Meeting_Friends.cpp

--- Problemas/723A - New Year Meeting Friends/723A_New_Year_Meeting_Friends.cpp
+++ Problemas/723A - New Year Meeting Friends/723A_New_Year_Meeting_Friends.cpp
@@ -10,14 +10,14 @@ using namespace std;
 int main(){
 
     // Input:
-    // 1) Coordenadas de las casas de los amigos
-    int amigos[3];
+    // 1) coordenadas_amigos = Coordenadas de las casas de los amigos
+    int coordenadas_amigos[3];
     for(int amigo = 0; amigo < 3; amigo++) {
-        cin >> amigos[amigo];
+        cin >> coordenadas_amigos[amigo];
     }
 
     // Resta la coordenada más alta y baja de las 3 para calcular la distancia mínima
-    int distancia = *max_element(amigos, amigos+3) - *min_element(amigos, amigos+3);
+    int distancia = *max_element(coordenadas_amigos, coordenadas_amigos+3) - *min_element(coordenadas_amigos, coordenadas_amigos+3);
 
     // Imprime la distancia recorrida
     cout << distancia << endl;
