29529a0387a1aa06a2e2363e18e64164bb7e14a3
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 18:28:01 -0600

    Update 791A_Bear_and_Big_Brother.py

--- Problemas/791A - Bear and Big Brother/791A_Bear_and_Big_Brother.py
+++ Problemas/791A - Bear and Big Brother/791A_Bear_and_Big_Brother.py
@@ -1,20 +1,20 @@
 # 791A - Bear and Big Brother
 # https://codeforces.com/problemset/problem/791/A
 
-# Inputs:
-# 1) Peso de Limak (a) 
-# 2) Peso de Bob (b)
-limak, bob = map(int, input().split())
+# Input:
+# 1) peso_limak = Peso de Limak
+# 2) peso_bob = Peso de Bob 
+peso_limak, peso_bob = map(int, input().split())
 
-# Numero de años pasados
+# Numero de años transcurridos
 anios = 0
 
 # Mientras Limak pese menos que Bob:
 # Suma 1 año, multiplica x3 el peso de Limak y x2 el de Bob
-while limak <= bob:
+while peso_limak <= peso_bob:
     anios += 1
-    limak = limak * 3
-    bob = bob * 2
+    peso_limak = peso_limak * 3
+    peso_bob = peso_bob * 2
 
 # Imprime el número de años que pasaron para que Limak pesara más que Bob
 print(anios)
