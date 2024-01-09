6e1cb1b13f9bde6481b82d61f97a2d7d7b5e40b6
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 18:47:56 -0600

    Update 750A_New_Year_And_Hurry.cpp

--- Problemas/750A - New Year And Hurry/750A_New_Year_And_Hurry.cpp
+++ Problemas/750A - New Year And Hurry/750A_New_Year_And_Hurry.cpp
@@ -10,13 +10,13 @@ using namespace std;
 int main(){
 
     // Input:
-    // 1) Número de problemas
-    // 2) Tiempo de viaje a la fiesta
+    // 1) problemas = Número de problemas
+    // 2) minutos_viaje = Tiempo de viaje a la fiesta
     int problemas, minutos_viaje;
     cin >> problemas >> minutos_viaje;
 
     // ¿Cuánto tiempo queda para resolver los problemas del concurso?
-    int tiempo_restante = 240 - minutos_viaje;
+    int tiempo_concurso = 240 - minutos_viaje;
 
     // ¿Cuántos problemas del concurso has resuelto?
     int problemas_resueltos = 0;
@@ -24,14 +24,14 @@ int main(){
     // Itera sobre los problemas
     for(int numero_problema_actual = 1; numero_problema_actual <= problemas; numero_problema_actual++) {
 
-        // Si al resolver el problema actual te sobra tiempo, entonces resuélvelo, suma +1 al número de problemas resueltos y actualiza el tiempo restante
-        if(tiempo_restante - (5 * numero_problema_actual) >= 0) {
-            tiempo_restante = tiempo_restante - (5 * numero_problema_actual);
+        // Si al resolver el problema actual el tiempo del concurso es mayor a 0, entonces resuélvelo, suma +1 al número de problemas resueltos y actualiza el tiempo restante
+        if(tiempo_concurso - (5 * numero_problema_actual) >= 0) {
+            tiempo_concurso = tiempo_concurso - (5 * numero_problema_actual);
             problemas_resueltos += 1;
         }
 
-        // Si al resolver el problema actual te falta tiempo, entonces termina de iterar sobre los problemas
-        else if(tiempo_restante - (5 * numero_problema_actual) < 0) {
+        // Si al resolver el problema actual el tiempo del concurso es menor a 0, entonces termina de iterar sobre los problemas
+        else if(tiempo_concurso - (5 * numero_problema_actual) < 0) {
             break;
         }
     }
