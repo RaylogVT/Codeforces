3cc6d4de1c6225e194af6ee945cc6bda153c0efa
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 20:04:44 -0600

    Update 50A_Domino_Piling.py

--- Problemas/50A - Domino Piling/50A_Domino_Piling.py
+++ Problemas/50A - Domino Piling/50A_Domino_Piling.py
@@ -1,9 +1,9 @@
 # 50A - Domino Piling
 # https://codeforces.com/problemset/problem/50/A
 
-# Inputs:
-# m = Ancho
-# n = Alto
+# Input:
+# 1) m = Ancho
+# 2) n = Alto
 m, n = [int(x) for x in input().split()]
 
 # Calcula el número de dominós 
