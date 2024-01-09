6ffa0f8e74bbfb14d37a9abab56c4acdc84a9a55
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 19:27:37 -0600

    Update 61A_Ultra_Fast_Mathematician.cpp

--- Problemas/61A - Ultra-Fast Mathematician/61A_Ultra_Fast_Mathematician.cpp
+++ Problemas/61A - Ultra-Fast Mathematician/61A_Ultra_Fast_Mathematician.cpp
@@ -7,43 +7,11 @@
 #include <cmath>
 using namespace std;
 
-/*
 int main(){
 
     // Input:
-    // 1) Número 1
-    // 2) Número 2
-    string numero1, numero2;
-    
-    // cin >> numero1 >> numero2;
-    
-    while(cin >> numero1 >> numero2) {
-
-        // Longitud de los dos números
-        int longitud_numero = numero1.length();
-
-        // Reusltado de la operación
-        string resultado = "";
-
-        for(int digito = 0; digito < longitud_numero; digito++) {
-            if(numero1[digito] == numero2[digito]) { 
-                resultado.push_back('0');
-            } else {
-                resultado.push_back('1');
-            }
-        }
-        cout << resultado << endl;
-    }
-
-    return 0;
-}
-*/
-
-int main(){
-
-    // Input:
-    // 1) Número 1
-    // 2) Número 2
+    // 1) numero1 = Número 1
+    // 2) numero2 = Número 2
     string numero1, numero2;
     cin >> numero1 >> numero2;
     
@@ -56,8 +24,8 @@ int main(){
     // Itera sobre los dígitos de los números 
     for(int digito = 0; digito < longitud_numero; digito++) {
 
-        // Si los dígitos son iguales, entonces la respuesta es 0
-        // Si los dígitos son distintos, entonces la respuesta es 1
+        // Si los dígitos son iguales, entonces la respuesta es 0, añádelo al resultado
+        // Si los dígitos son distintos, entonces la respuesta es 1, añádelo al resultado
         if(numero1[digito] == numero2[digito]) { 
             resultado.push_back('0');
         } else {
