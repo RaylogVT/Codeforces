04149d76fbe25b3bd29a9ec5c56d614b64e08499
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 20:49:43 -0600

    Update 266A_Stones_on_the_Table.cpp

--- Problemas/266A - Stones on the Table/266A_Stones_on_the_Table.cpp
+++ Problemas/266A - Stones on the Table/266A_Stones_on_the_Table.cpp
@@ -9,22 +9,23 @@ using namespace std;
 int main(){
 
     // Input:
-    // 1) Número de piedras
+    // 1) num_piedras = Número de piedras
     int num_piedras;
     cin >> num_piedras;
 
     // Input:
-    // 2) Colores de piedras
+    // 2) RGB = Colores de piedras
     string RGB;
     cin >> RGB;
 
     // Número de piedras nuevas a añadir
     int nuevas_piedras = 0;
 
-    // Compara dos piedras vecinas
+    // Itera sobre las piedras
     for(int piedra = 1; piedra <= num_piedras; piedra++) {
-
-        // Si son iguales, añade una nueva piedra
+        
+        // Compara dos piedras vecinas
+        // Si son iguales, añade una nueva piedra, suma +1 contador de piedras nuevas
         if(RGB[piedra] == RGB[piedra-1]) {
             nuevas_piedras++;
         }
