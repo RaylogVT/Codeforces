712a874ff6ff9015881f092683487cc69b14a24c
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 18:20:50 -0600

    Update 996A_Hit_The_Lottery.py

--- Problemas/996A - Hit The Lottery/996A_Hit_The_Lottery.py
+++ Problemas/996A - Hit The Lottery/996A_Hit_The_Lottery.py
@@ -2,7 +2,7 @@
 # https://codeforces.com/problemset/problem/996/A
 
 # Inputs:
-# 1) Dinero a sacar
+# 1) dinero = Dinero a sacar
 dinero = int(input())
 
 # Denominaciones de billetes
@@ -11,16 +11,16 @@ denominaciones = [1,5,10,20,100]
 # Cantidad total de billetes a sacar
 billetes_total = 0
 
-for denom_indice in range(len(denominaciones)-1, -1, -1):
+for denominacion_indice in range(len(denominaciones)-1, -1, -1):
 
     # Cantidad de billetes a sacar con la denominación actual
-    billetes_actual = int(dinero / denominaciones[denom_indice])
+    billetes_actual = int(dinero / denominaciones[denominacion_indice])
 
     # Añade los nuevos billetes a tu cantidad final
     billetes_total = billetes_total + billetes_actual
 
     # Calcula el dinero que te queda por sacar con otras denominaciones
-    dinero = dinero - (denominaciones[denom_indice] * billetes_actual)
+    dinero = dinero - (denominaciones[denominacion_indice] * billetes_actual)
 
 # Imprime la cantidad de billetes total que necesitas para sacar el dinero
 print(billetes_total)
