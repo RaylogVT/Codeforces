88d348e26d34c7dfe2bfb69c31d25dc5122a14b5
RaylogVT <129716395+RaylogVT@users.noreply.github.com>
2024-01-08 18:39:46 -0600

    Update 750A_New_Year_And_Hurry.py

--- Problemas/750A - New Year And Hurry/750A_New_Year_And_Hurry.py
+++ Problemas/750A - New Year And Hurry/750A_New_Year_And_Hurry.py
@@ -2,12 +2,12 @@
 # https://codeforces.com/problemset/problem/750/A
 
 # Input:
-# 1) Número de problemas
-# 2) Tiempo de viaje a la fiesta
+# 1) problemas = Número de problemas
+# 2) minutos_viaje = Tiempo de viaje a la fiesta
 problemas, minutos_viaje = map(int, input().split())
 
 # ¿Cuánto tiempo queda para resolver los problemas del concurso?
-tiempo_restante = 240 - minutos_viaje
+tiempo_concurso = 240 - minutos_viaje
 
 # ¿Cuántos problemas del concurso has resuelto?
 problemas_resueltos = 0
@@ -15,13 +15,13 @@ problemas_resueltos = 0
 # Itera sobre los problemas
 for numero_problema_actual in range(1, problemas+1):
 
-    # Si al resolver el problema actual te sobra tiempo, entonces resuélvelo, suma +1 al número de problemas resueltos y actualiza el tiempo restante
-    if tiempo_restante - (5 * numero_problema_actual) >= 0:
-        tiempo_restante = tiempo_restante - (5 * numero_problema_actual)
+    # Si al resolver el problema actual el tiempo del concurso es mayor a 0, entonces resuélvelo, suma +1 al número de problemas resueltos y actualiza el tiempo restante del concurso
+    if tiempo_concurso - (5 * numero_problema_actual) >= 0:
+        tiempo_concurso = tiempo_concurso - (5 * numero_problema_actual)
         problemas_resueltos += 1
 
-    # Si al resolver el problema actual te falta tiempo, entonces termina de iterar sobre los problemas
-    elif tiempo_restante - (5 * numero_problema_actual) < 0:
+    # Si al resolver el problema actual el tiempo del concurso es menor a 0, entonces termina de iterar sobre los problemas
+    elif tiempo_concurso - (5 * numero_problema_actual) < 0:
         break
 
 # Imprime el número de problemas resueltos
